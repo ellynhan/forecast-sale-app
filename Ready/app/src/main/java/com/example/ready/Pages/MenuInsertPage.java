cf770fe1735c71e46428a289cab731089e7a08df
redundant4u <rafch@naver.com>
2021-05-28 01:22:28 +0900

    [S] Fix menu insert & delete method

--- Ready/app/src/main/java/com/example/ready/Pages/MenuInsertPage.java
+++ Ready/app/src/main/java/com/example/ready/Pages/MenuInsertPage.java
@@ -1,6 +1,5 @@
 package com.example.ready.Pages;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -21,9 +20,9 @@ import java.util.ArrayList;
 public class MenuInsertPage extends Fragment {
     private View v;
     private Button addBtn, editBtn;
-    private TableLayout rootTable;
+    private TableLayout rootTable, deleteTable;
     private DBHelper dbHelper;
-    private Boolean ISDELETE = false;
+    private Boolean isDelete = false, isAdd = false;
 
     private ArrayList<Button> deleteBtns = new ArrayList<>();
     private ArrayList<Integer> deleteIndex = new ArrayList<>();
@@ -39,27 +38,27 @@ public class MenuInsertPage extends Fragment {
         v = inflater.inflate(R.layout.menu_page, container, false);
 
         rootTable = v.findViewById(R.id.tableLayout);
+        deleteTable = v.findViewById(R.id.menu_delete_layout);
         dbHelper = DBHelper.getInstance(v.getContext());
 
-        menus = dbHelper.getMenu();
-        for(int i = 0; i < menus.size(); i++)
-            deleteIndex.add(0);
-
+        dbInit();
         tableLayoutInit();
-        buttonInit();
+        deleteButtonInit();
+        addButtonInit();
+
+        editBtn = v.findViewById(R.id.btn_menu_edit);
+        editBtn.setOnClickListener(editOnClickListener);
 
         return v;
     }
 
-    private void tableLayoutInit() {
-//        try {
-//            System.out.println(menus.size());
-//            rootTable.removeViews(1, menus.size());
-//        }
-//        catch(Exception e) {
-//            e.printStackTrace();
-//        }
+    private void dbInit() {
+        menus = dbHelper.getMenu();
+        for(int i = 0; i < menus.size(); i++)
+            deleteIndex.add(0);
+    }
 
+    private void tableLayoutInit() {
         // insert data to table
         for(int i = 0; i < menus.size(); i++) {
             TableRow tableRow = new TableRow(v.getContext());
@@ -90,12 +89,8 @@ public class MenuInsertPage extends Fragment {
         }
     }
 
-    private void buttonInit() {
-        editBtn = v.findViewById(R.id.btn_menu_edit);
-
+    private void deleteButtonInit() {
         final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        final LinearLayout menuLayout = v.findViewById(R.id.menu_layout);
-        final TableLayout deleteTable = v.findViewById(R.id.menu_delete_layout);
         final TableLayout.LayoutParams tlParams = new TableLayout.LayoutParams(
                 TableLayout.LayoutParams.WRAP_CONTENT,
                 TableLayout.LayoutParams.WRAP_CONTENT
@@ -107,11 +102,10 @@ public class MenuInsertPage extends Fragment {
 
         // button margin options
         tlParams.topMargin = Math.round(50 * displayMetrics.density);
-        tlParams.leftMargin = Math.round(-40 * displayMetrics.density);
+        tlParams.leftMargin = Math.round(-35 * displayMetrics.density);
         trParams.bottomMargin = Math.round(14 * displayMetrics.density);
 
         // add delete button in view
-        System.out.println("menus: " + menus.size());
         deleteBtns.clear();
         for (int i = 0; i < menus.size(); i++) {
             TableRow tableRow = new TableRow(v.getContext());
@@ -130,6 +124,13 @@ public class MenuInsertPage extends Fragment {
             deleteBtns.add(deleteBtnTmp);
         }
 
+        for(int i = 0; i < deleteBtns.size(); i++)
+            deleteBtns.get(i).setOnClickListener(deleteClickListener);
+    }
+
+    private void addButtonInit() {
+        final LinearLayout menuLayout = v.findViewById(R.id.menu_layout);
+
         // add 'add' button in view
         final Button addBtnTmp = new Button(v.getContext());
         final LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
@@ -146,29 +147,15 @@ public class MenuInsertPage extends Fragment {
         addBtn = v.findViewWithTag("addBtn");
         addBtn.setVisibility(Button.INVISIBLE);
 
-        editBtn.setOnClickListener(editOnClickListener);
         addBtn.setOnClickListener(addOnClickListener);
-
-        for(int i = 0; i < deleteBtns.size(); i++)
-            deleteBtns.get(i).setOnClickListener(deleteClickListener);
     }
 
     private View.OnClickListener editOnClickListener = new View.OnClickListener() {
-        boolean isEdit = false;
+        private Boolean isEdit = false;
 
         @Override
         public void onClick(View v) {
-            if (!isEdit) {
-                editBtn.setText("편집 저장");
-
-                for (int i = 0; i < menus.size(); i++)
-                    deleteBtns.get(i).setVisibility(Button.VISIBLE);
-                addBtn.setVisibility(Button.VISIBLE);
-
-                isEdit = true;
-            }
-
-            else {
+            if(isEdit) {
                 editBtn.setText("메뉴 편집");
 
                 // button invisible
@@ -176,35 +163,62 @@ public class MenuInsertPage extends Fragment {
                     deleteBtns.get(i).setVisibility(View.INVISIBLE);
                 addBtn.setVisibility(View.INVISIBLE);
 
-                // delete editText & db insert
-                for (int i = 0, j = 0; i < menuAddRow.size(); i++, j += 2) {
-                    menuAddRow.get(i).setVisibility(EditText.GONE);
+                if(isAdd) {
+                    // delete editText & db insert
+                    for (int i = 0, j = 0; i < menuAddRow.size(); i++, j += 2) {
+                        menuAddRow.get(i).setVisibility(EditText.GONE);
 
-                    try {
-                        Menu menu = new Menu(
-                                0,
-                                menuEditText.get(j).getText().toString(),
-                                Integer.parseInt(menuEditText.get(j + 1).getText().toString())
-                        );
-                        dbHelper.insertMenu(menu);
-                        System.out.println("DB INSERT");
+                        try {
+                            Menu menu = new Menu(
+                                    0,
+                                    menuEditText.get(j).getText().toString(),
+                                    Integer.parseInt(menuEditText.get(j + 1).getText().toString())
+                            );
+                            dbHelper.insertMenu(menu);
+                            System.out.println("DB INSERT");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            System.out.println("DB INSERT FAIL");
+                        }
                     }
-                    catch(Exception e) {
-                        e.printStackTrace();
-                        System.out.println("DB INSERT FAIL");
-                    }
+
+                    // menu & button layout update
+                    deleteTable.removeViews(0, menus.size());
+                    dbInit();
+                    rootTable.removeViews(1, menus.size());
+                    tableLayoutInit();
+                    deleteButtonInit();
+
+                    isAdd = false;
                 }
 
                 //db delete
-                if(ISDELETE) {
+                if(isDelete) {
                     dbHelper.deleteMenu(deleteIndex);
+
+                    deleteTable.removeViews(0, menus.size());
+                    rootTable.removeViews(1, menus.size());
+                    dbInit();
+                    tableLayoutInit();
+                    deleteButtonInit();
+
                     System.out.println("DB DELETE");
                 }
 
                 menuAddRow.clear();
                 menuEditText.clear();
-
+                isDelete = false;
                 isEdit = false;
+            }
+
+            else {
+                editBtn.setText("편집 저장");
+
+                for (int i = 0; i < menus.size(); i++)
+                    deleteBtns.get(i).setVisibility(Button.VISIBLE);
+                addBtn.setVisibility(Button.VISIBLE);
+
+                isEdit = true;
             }
         }
     };
@@ -231,6 +245,7 @@ public class MenuInsertPage extends Fragment {
 
             menuAddRow.add(tableRow);
             rootTable.addView(tableRow);
+            isAdd = true;
         }
     };
 
@@ -243,12 +258,12 @@ public class MenuInsertPage extends Fragment {
             if(deleteIndex.get(id) == 0) {
                 deleteIndex.set(id, menus.get(id)._id);
                 row.setBackgroundResource(R.drawable.menu_delete_shape);
-                ISDELETE = true;
+                isDelete = true;
             }
             else {
                 deleteIndex.set(id, 0);
                 row.setBackgroundResource(R.drawable.menu_insert_shape);
-                ISDELETE = false;
+                isDelete = false;
             }
         }
     };
