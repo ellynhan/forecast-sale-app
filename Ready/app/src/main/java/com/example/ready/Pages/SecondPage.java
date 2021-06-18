797c4eb4cc9002ae4b8270dfe723f4def6fbf4ac
redundant4u <rafch@naver.com>
2021-06-18 12:22:02 +0900

    [S] Add RadioButton

--- Ready/app/src/main/java/com/example/ready/Pages/SecondPage.java
+++ Ready/app/src/main/java/com/example/ready/Pages/SecondPage.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,57 +15,85 @@ import com.example.ready.Calendar.GuessDecorator;
 import com.example.ready.Calendar.SaturdayDecorator;
 import com.example.ready.Calendar.SundayDecorator;
 import com.example.ready.Calendar.WeekDayDecorator;
+import com.example.ready.DB.DBHelper;
+import com.example.ready.DB.Model.Menu;
+import com.example.ready.DB.Model.Sale;
 import com.example.ready.R;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class SecondPage extends Fragment {
+    private ArrayList<Menu> menus;
+    private ArrayList<Sale> sales;
+
+    private View v;
     private MaterialCalendarView materialCalendarView;
     private SlidingUpPanelLayout slidingUpPanelLayout;
-
-    Calendar calendar = Calendar.getInstance();
-    CalendarDay date = CalendarDay.from(calendar);
+    private DBHelper dbHelper;
+    private RadioGroup radioGroup;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.second_page, container, false);
+        v = inflater.inflate(R.layout.second_page, container, false);
 
-        // for test
-        calendar.add(Calendar.DAY_OF_YEAR, 1);
-        CalendarDay date2 = CalendarDay.from(calendar);
-        calendar.add(Calendar.DAY_OF_YEAR, 4);
-        CalendarDay date3 = CalendarDay.from(calendar);
+        dbHelper = DBHelper.getInstance(v.getContext());
 
-        // calendar decorator
         materialCalendarView = v.findViewById(R.id.calendarView);
+        slidingUpPanelLayout = v.findViewById(R.id.sliding_layout);
+        radioGroup = v.findViewById(R.id.radioGroup);
+
+        calendarInit();
+        calendarDataLoad();
+        radioGroupInit();
+
+        return v;
+    }
+
+    private void calendarInit() {
         materialCalendarView.setSelectedDate(CalendarDay.today());
         materialCalendarView.addDecorators(
                 new SaturdayDecorator(),
                 new SundayDecorator(),
                 new WeekDayDecorator()
         );
-        materialCalendarView.addDecorators(
-                new GuessDecorator("20", Color.RED, Collections.singleton(date)),
-                new GuessDecorator("10", Color.BLUE, Collections.singleton(date2)),
-                new GuessDecorator("5", Color.GREEN, Collections.singleton(date3))
-        );
-
-        // when date is clicked, show up slidingUpPanelLayout
-        slidingUpPanelLayout = v.findViewById(R.id.sliding_layout);
         materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
                 slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
             }
         });
+    }
 
+    private void calendarDataLoad() {
+        // sales = dbHelper.getSale();
+//
+//        materialCalendarView.addDecorators(
+//                new GuessDecorator("20", Color.RED, Collections.singleton(date)),
+//                new GuessDecorator("30", Color.RED, Collections.singleton(date)),
+//                new GuessDecorator("10", Color.BLUE, Collections.singleton(date2)),
+//                new GuessDecorator("5", Color.GREEN, Collections.singleton(date3))
+//        );
+    }
 
-        return v;
+    private void radioGroupInit() {
+        menus = dbHelper.getMenu();
+
+        for(int i = 0; i < menus.size(); i++) {
+            RadioButton radioButton = new RadioButton(v.getContext());
+            radioButton.setLayoutParams(new RadioGroup.LayoutParams(
+                    RadioGroup.LayoutParams.WRAP_CONTENT,
+                    RadioGroup.LayoutParams.WRAP_CONTENT
+            ));
+            radioButton.setId(menus.get(i)._id);
+            radioButton.setText(menus.get(i).menu_name);
+
+            radioGroup.addView(radioButton);
+        }
     }
 }
