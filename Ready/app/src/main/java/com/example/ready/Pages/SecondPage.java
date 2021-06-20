8f643b45b33be4e391d4271f1a366e8b4652af33
redundant4u <rafch@naver.com>
2021-06-20 15:58:33 +0900

    [S] Design slideuppanel

--- Ready/app/src/main/java/com/example/ready/Pages/SecondPage.java
+++ Ready/app/src/main/java/com/example/ready/Pages/SecondPage.java
@@ -17,13 +17,18 @@ import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class SecondPage extends Fragment {
     private ArrayList<Menu> menus;
+    private ArrayList<String> menuList = new ArrayList<>();
 
+    private int menu_id;
     private View v;
+    private TextView day, weather, menu_name, predict_value;
     private MaterialCalendarView materialCalendarView;
     private SlidingUpPanelLayout slidingUpPanelLayout;
     private DBHelper dbHelper;
@@ -39,6 +44,11 @@ public class SecondPage extends Fragment {
         materialCalendarView = v.findViewById(R.id.calendarView);
         slidingUpPanelLayout = v.findViewById(R.id.sliding_layout);
         spinner = v.findViewById(R.id.spinner_menu);
+
+        day = v.findViewById(R.id.day);
+        weather = v.findViewById(R.id.weather);
+        menu_name = v.findViewById(R.id.menu_name);
+        predict_value = v.findViewById(R.id.predict_value);
 
         calendarInit();
         spinnerInit();
@@ -57,14 +67,13 @@ public class SecondPage extends Fragment {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
                 slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-                panelInit();
+                panelInit(date);
             }
         });
     }
 
     private void spinnerInit() {
         menus = dbHelper.getMenu();
-        ArrayList<String> menuList = new ArrayList<>();
 
         menuList.add("메뉴 선택");
         for(int i = 0; i < menus.size(); i++)
@@ -72,7 +81,7 @@ public class SecondPage extends Fragment {
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
                 v.getContext(),
-                R.layout.spinner_list,
+                R.layout.spinner_list_black,
                 menuList
         );
 
@@ -82,31 +91,33 @@ public class SecondPage extends Fragment {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                int menu_id = (int) id;
+                menu_id = (int) id;
                 System.out.println(menu_id);
 
                 if(menu_id != 0) {
+                    ArrayList<String> dates = dbHelper.getSaleDateWithId(menu_id);
+
                     materialCalendarView.removeDecorators();
                     materialCalendarView.invalidateDecorators();
                     materialCalendarView.addDecorators(
                             new SaturdayDecorator(),
                             new SundayDecorator(),
-                            new WeekDayDecorator()
+                            new WeekDayDecorator(),
+                            new GuessDecorator(dates)
                     );
 
-                    ArrayList<String> dates = dbHelper.getSaleDateWithId(menu_id);
-                    ArrayList<Integer> qtys = dbHelper.getSaleQtyWithId(menu_id);
+//                    ArrayList<Integer> qtys = dbHelper.getSaleQtyWithId(menu_id);
 
-                    for(int i = 0; i < dates.size(); i++) {
-                        String date = dates.get(i);
-                        int qty = qtys.get(i);
-
-                        System.out.println("DATE " + date + " QTY " + qty);
-
-                        materialCalendarView.addDecorator(
-                                new GuessDecorator(date, qty)
-                        );
-                    }
+//                    for(int i = 0; i < dates.size(); i++) {
+//                        String date = dates.get(i);
+//                        int qty = qtys.get(i);
+//
+//                        System.out.println("DATE " + date + " QTY " + qty);
+//
+//                        materialCalendarView.addDecorator(
+//                                new GuessDecorator(date, qty)
+//                        );
+//                    }
                 }
             }
 
@@ -115,7 +126,31 @@ public class SecondPage extends Fragment {
         });
     }
 
-    private void panelInit() {
+    private void panelInit(CalendarDay date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+        String dateString = simpleDateFormat.format(date.getDate());
 
+        day.setText(dateString);
+
+        if(menu_id != 0) {
+            menu_name.setText(menuList.get(menu_id) + " ");
+
+            ArrayList<Integer> sale = dbHelper.getSaleQtySkyWithIdAndDate(menu_id ,dateString);
+            predict_value.setText(String.valueOf(sale.get(0)));
+
+            String sky = "";
+            switch(sale.get(1)) {
+                case 0:
+                    sky = "맑음";
+                    break;
+                case 1:
+                    sky = "구름많음";
+                    break;
+                case 2:
+                    sky = "흐름";
+                    break;
+            }
+            weather.setText(sky);
+        }
     }
 }
